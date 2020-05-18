b26639386772b637fd78fbe9ee3470fc970c2a91
Kevin <kevin.lau.gd@gmail.com>
2020-05-18 19:16:56 +0800

    不强制要求填写某项配置

--- src/TradingSystem/Config.h
+++ src/TradingSystem/Config.h
@@ -16,15 +16,16 @@ inline bool load_login_params(const std::string& file,
   if (!ifs) return false;
 
   YAML::Node config = YAML::LoadFile(file);
-  params->set_api(config["api"].as<std::string>());
-  params->set_front_addr(config["front_addr"].as<std::string>());
-  params->set_md_server_addr(config["md_server_addr"].as<std::string>());
-  params->set_broker_id(config["broker_id"].as<std::string>());
-  params->set_investor_id(config["investor_id"].as<std::string>());
-  params->set_passwd(config["passwd"].as<std::string>());
-  params->set_auth_code(config["auth_code"].as<std::string>());
-  params->set_app_id(config["app_id"].as<std::string>());
-  params->set_subscribed_list({config["ticker"].as<std::string>()});
+
+  params->set_api(config["api"].as<std::string>(""));
+  params->set_front_addr(config["front_addr"].as<std::string>(""));
+  params->set_md_server_addr(config["md_server_addr"].as<std::string>(""));
+  params->set_broker_id(config["broker_id"].as<std::string>(""));
+  params->set_investor_id(config["investor_id"].as<std::string>(""));
+  params->set_passwd(config["passwd"].as<std::string>(""));
+  params->set_auth_code(config["auth_code"].as<std::string>(""));
+  params->set_app_id(config["app_id"].as<std::string>(""));
+  params->set_subscribed_list({config["ticker"].as<std::string>("")});
 
   return true;
 }
