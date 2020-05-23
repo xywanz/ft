f8192b1bde814e45d7321a35d86c36a885c9aeb1
Kevin <kevin.lau.gd@gmail.com>
2020-05-23 21:08:51 +0800

    暂时先移除对CTP gb2312的转码，因为codecvt好像并不是所有编译器都支持，在travis里面就没有通过编译，后续考虑其他方式转码

--- src/Gateway/Ctp/CtpCommon.h
+++ src/Gateway/Ctp/CtpCommon.h
@@ -5,7 +5,7 @@
 
 #include <ThostFtdcUserApiDataType.h>
 
-#include <codecvt>
+// #include <codecvt>
 #include <limits>
 #include <locale>
 #include <map>
@@ -36,22 +36,23 @@ inline bool is_error_rsp(CThostFtdcRspInfoField* rsp_info) {
 }
 
 inline std::string gb2312_to_utf8(const std::string& gb2312) {
-  static const std::locale loc("zh_CN.GB2312");
+  // static const std::locale loc("zh_CN.GB2312");
 
-  std::vector<wchar_t> wstr(gb2312.size());
-  wchar_t* wstr_end = nullptr;
-  const char* gb_end = nullptr;
-  mbstate_t state{};
-  int res = std::use_facet<std::codecvt<wchar_t, char, mbstate_t>>(loc).in(
-      state, gb2312.data(), gb2312.data() + gb2312.size(), gb_end, wstr.data(),
-      wstr.data() + wstr.size(), wstr_end);
+  // std::vector<wchar_t> wstr(gb2312.size());
+  // wchar_t* wstr_end = nullptr;
+  // const char* gb_end = nullptr;
+  // mbstate_t state{};
+  // int res = std::use_facet<std::codecvt<wchar_t, char, mbstate_t>>(loc).in(
+  //     state, gb2312.data(), gb2312.data() + gb2312.size(), gb_end,
+  //     wstr.data(), wstr.data() + wstr.size(), wstr_end);
 
-  if (res == std::codecvt_base::ok) {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> cutf8;
-    return cutf8.to_bytes(std::wstring(wstr.data(), wstr_end));
-  }
+  // if (res == std::codecvt_base::ok) {
+  //   std::wstring_convert<std::codecvt_utf8<wchar_t>> cutf8;
+  //   return cutf8.to_bytes(std::wstring(wstr.data(), wstr_end));
+  // }
 
-  return "";
+  // return "";
+  return gb2312;
 }
 
 template <class PriceType>
