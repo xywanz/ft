7ecbd5dd972d53ade1ebb4a5804372f382aad4f5
Kevin <kevin.lau.gd@gmail.com>
2020-05-29 00:13:04 +0800

    rename

--- include/utils/misc.h
+++
@@ -1,8 +0,0 @@
-// Copyright [2020] <Copyright Kevin, kevin.lau.gd@gmail.com>
-
-#ifndef FT_INCLUDE_UTILS_MISC_H_
-#define FT_INCLUDE_UTILS_MISC_H_
-
-#define UNUSED(x) ((void)(x))
-
-#endif  // FT_INCLUDE_UTILS_MISC_H_
