d8f7119600df62d5d856d31f324e67e80d625e4a
Kevin <kevin.lau.gd@gmail.com>
2021-02-01 21:31:02 +0800

    调整cmake结构

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -10,20 +10,20 @@ git clone https://github.com/redis/hiredis.git --depth=1
 mkdir -p yaml-cpp/build && cd yaml-cpp/build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make -j8
 cd ../..
-cp -rf yaml-cpp/include ../dependencies/yaml-cpp/
-cp -rf yaml-cpp/build/libyaml-cpp.a ../dependencies/yaml-cpp/lib/
+cp -rf yaml-cpp/include ../third_party/yaml-cpp/
+cp -rf yaml-cpp/build/libyaml-cpp.a ../third_party/yaml-cpp/lib/
 
 mkdir -p spdlog/build && cd spdlog/build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make -j8
 cd ../..
-cp -rf spdlog/include ../dependencies/spdlog/
-cp -rf spdlog/build/libspdlog.a ../dependencies/spdlog/lib/
+cp -rf spdlog/include ../third_party/spdlog/
+cp -rf spdlog/build/libspdlog.a ../third_party/spdlog/lib/
 
 mkdir -p hiredis/build && cd hiredis/build
 cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make -j8
 cd ../..
-cp -rf hiredis/*.h ../dependencies/hiredis/include
-cp -rf hiredis/build/libhiredis.so* ../dependencies/hiredis/lib/
+cp -rf hiredis/*.h ../third_party/hiredis/include
+cp -rf hiredis/build/libhiredis.so* ../third_party/hiredis/lib/
 
 cd ..
 rm -rf update
